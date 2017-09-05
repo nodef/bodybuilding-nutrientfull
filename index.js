69dc98499934c628b1ffd00f4b02f722495634de
Subhajit Sahu <wolfram77@gmail.com>
2017-09-05 16:27:40 +0530

    :two_hearts: use jsdom.fromurl closes #2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 const https = require('https');
-const JsDom = require('jsdom');
+const jsdom = require('jsdom');
 const _assign = require('lodash.assign');
 
 const text = function(td) {
@@ -9,35 +9,24 @@ const text = function(td) {
   return b.length? b[0].innerHTML : font[0].innerHTML;
 };
 
+const request = function(path) {
+  return jsdom.JSDOM.fromURL(`https://www.bodybuilding.com${path}`, {
+    'userAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+  });
+};
+
 const $ = function(id) {
-  return new Promise((fres, frej) => {
-    const req = https.request({
-      'hostname': 'www.bodybuilding.com',
-      'path': `/fun/nutrientfull.php?id=${id}`,
-      'headers': {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    }, (res) => {
-      var data = '';
-      if(res.statusCode!==200) {
-        res.resume();
-        return frej(new Error(`Bad Response: ${res.statusCode}`));
-      }
-      res.setEncoding('utf8');
-      res.on('data', (chunk) => data += chunk);
-      res.on('end', () => {
-        const a = {}, b = {};
-        const doc = new JsDom.JSDOM(data).window.document;
-        const tables = doc.getElementsByTagName('table');
-        if(tables.length<3) return fres({});
-        for(var tr of tables[2].getElementsByTagName('tr')) {
-          var tds = tr.getElementsByTagName('td');
-          if(tds.length===2) b[text(tds[0])] = text(tds[1]);
-        }
-        a[text(tables[1])] = b;
-        fres(a);
-      });
-    });
-    req.on('error', (err) => frej(err));
-    req.end();
+  return request(`/fun/nutrientfull.php?id=${id}`).then((dom) => {
+    const a = {}, b = {};
+    const document = dom.window.document;
+    const tables = document.getElementsByTagName('table');
+    if(tables.length<3) return fres({});
+    for(var tr of tables[2].getElementsByTagName('tr')) {
+      var tds = tr.getElementsByTagName('td');
+      if(tds.length===2) b[text(tds[0])] = text(tds[1]);
+    }
+    a[text(tables[1])] = b;
+    return a;
   });
 };
 module.exports = $;
